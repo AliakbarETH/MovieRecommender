0482a6dc68a77504a87b339b5e2781a2d5e0dbaa
Ali Akbar <aliakbar.eth@gmail.com>
2025-06-23 22:57:21 +0100

    Completed dataModel.py using pydantic for movies, belief_data, user_rating_history files

---
+++ src/models.py
@@ -0,0 +1,55 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime
+from enum import Enum
+
+
+# defining data model for movies.csv file
+
+class Genre(Enum):
+    Action = "Action" 
+    Adventure = "Adventure" 
+    Animation = "Animation"
+    Children = "Children's"
+    Comedy = "Comedy"
+    Crime = "Crime"
+    Documentary = "Documentary"
+    Drama = "Drama"
+    Fantasy = "Fantasy"
+    FilmNoir = "Film-Noir"
+    Horror = "Horror"
+    Musical = "Musical"
+    Mystery = "Mystery"
+    Romance = "Romance"
+    SciFi = "Sci-Fi"
+    Thriller = "Thriller"
+    War = "War"
+    Western = "Western"
+
+class MoviesDataModel(BaseModel):
+    movieId : int
+    title : str
+    genere : List[Genre]
+    
+# defining data model for user_rating_history.csv file
+
+class UserRatingHistoryModel(BaseModel):
+    userId : int 
+    movidId : int  
+    rating : float 
+    tstamp : datetime
+
+# defining data model for belief_data.csv file
+
+class BeliefDataModel(BaseModel):
+    userId: int
+    movieId: int
+    isSeen: int  
+    watchDate: Optional[datetime]
+    userElicitRating: Optional[float]
+    userPredictRating: Optional[float]
+    userCertainty: Optional[float]
+    timestamp: datetime 
+    movie_idx: int
+    source: int
+    systemPredictRating: float
